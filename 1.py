ca7fe5f3a71b64d2e9e9f0c827ba7184a4c09e6e
Moaaz Mohamed <Moaazmoh@icloud.com>
2026-08-19 20:12:32 +0300

    added menu and 2 functions

--- 1.py
+++ 1.py
@@ -22,7 +22,21 @@ class library:
                     break
                 else:
                     print("Book Not Available")
-                    
+    def return_book(self):
+        self.show_books()
+        return_book=input("Enter The Book You Want To Return:")
+        for book in self.borrowed_books:
+         if return_book == book.title:
+            book.available = True
+            self.borrowed_books.remove(book)
+    def show_borrowed_books(self):
+      if not self.borrowed_books:
+        print("You have no borrowed books.")
+      else:
+        for book in self.borrowed_books:
+            print(book.title)   
+
+                   
                     
        
 
@@ -37,6 +51,22 @@ books = [book1, book2, book3, book4, book5]
 borrowed_books = []
 
 library = library(books, borrowed_books)
-library.show_books()
-print("test")
-        
+
+
+while(True):
+   print("1. Show Available Books\n2. Borrow Book\n3. Return Book\n4. Show Borrowed Books\n5. Exit")
+   choice=int(input("Enter Your Choice: "))
+   if choice==1:
+      library.show_books()
+   elif choice == 2:
+        library.borrow_book()
+
+   elif choice == 3:
+        library.return_book()
+
+   elif choice == 4:
+       library.show_borrowed_books()
+   elif choice == 5:
+        break  
+   else:
+       print("Invalid Input")
